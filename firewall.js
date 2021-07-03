0d2449c69a146fbc9c9584a65e9a15fd6b78d7d4
TheHickman <henry.hickmin@gmail.com>
2021-07-03 15:25:24 +1200

    Added animiation to packages

--- firewall.js
+++ firewall.js
@@ -4,13 +4,14 @@ let validSourceIP = randomIP();
 let portList = [23, 80, 443, 20, 21];
 
 window.onload = function() {
-	document.getElementById("score").innerHTML = 0;
-	document.getElementById("lives").innerHTML = 3;
-	document.getElementById("yourIP").innerHTML = validDestinationIP;
+	document.getElementById("scoreText").innerHTML = 0;
+	document.getElementById("livesText").innerHTML = 3;
+	document.getElementById("yourIPText").innerHTML = validDestinationIP;
 	for (let i = 0; i < 32; i++) {
 		invalidIPs.push(randomIP());
 	} 
 	chooseIP();
+	document.getElementById("portrait").className = "packageEntrance";
 };
 
 function randomIP() {
@@ -40,12 +41,22 @@ function chooseIP(score) {
 	}
 }
 
+function animation(status) {
+	var className = "package" + status;
+	document.getElementById("portrait").className = className;
+	setTimeout(function(){
+		document.getElementById("portrait").className = "packageEntrance";
+	}, 450)
+}
+
 function stage1check(button) {
 	var destination = document.getElementById("destinationIP").innerHTML;
 	if (destination == validDestinationIP && button == "allow") {
+		animation("Accept")
 		return true;
 	}
 	else if (destination != validDestinationIP && button == "deny") {
+		animation("Deny")
 		return true
 	}
 	else {
@@ -57,9 +68,11 @@ function stage2check(button) {
 	var destination = document.getElementById("destinationIP").innerHTML;
 	var source = document.getElementById("sourceIP").innerHTML;
 	if (destination == validDestinationIP && source == validSourceIP && button == 'allow') {
+		animation("Accept")
 		return true;
 	}
 	else if ((destination != validDestinationIP || source != validSourceIP) && button == 'deny') {
+		animation("Deny")
 		return true;
 	}
 	else {
@@ -68,8 +81,8 @@ function stage2check(button) {
 }
 
 function check(button) {
-	var currentScore = document.getElementById("score").innerHTML;
-	var newScore = document.getElementById("score").innerHTML;
+	var currentScore = document.getElementById("scoreText").innerHTML;
+	var newScore = document.getElementById("scoreText").innerHTML;
 	if (currentScore >= 5) {
 		result = stage2check(button);
 	}
@@ -80,10 +93,10 @@ function check(button) {
 		newScore ++;
 	}
 	else {
-		document.getElementById("lives").innerHTML --;
+		document.getElementById("livesText").innerHTML --;
 	}
 	if (currentScore != newScore) {
-		document.getElementById("score").innerHTML = newScore;
+		document.getElementById("scoreText").innerHTML = newScore;
 		chooseIP(currentScore);
 	}
 }
